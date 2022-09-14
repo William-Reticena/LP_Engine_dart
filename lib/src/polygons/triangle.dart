022c717006e7f279a44ceb7eb217e01e9f89a66a
GregullyWillian <gregullywillian@alunos.utfpr.edu.br>
2022-09-14 17:25:04 -0300

    triangle class

--- lib/src/polygons/triangle.dart
+++ lib/src/polygons/triangle.dart
@@ -1,1 +1,15 @@
-class Triangle {}
+import 'dart:io';
+
+class Triangle {
+  void create(int height, String simbol) {
+    for(int k = 1; k <= height; k++){
+      for(int j = 1; j <= height- k; j++){
+        stdout.write(' ');
+      }
+      for(int i = 1; i <= 2 * k - 1; i++){
+        stdout.write(simbol);
+      }
+      print('');
+    } 
+  }
+}
