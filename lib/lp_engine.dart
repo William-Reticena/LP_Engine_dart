babbae00a7b7d34d63d2b234f8112d0ef312fbf8
William Reticena <william.wsd43@gmail.com>
2022-09-18 21:20:45 -0300

    add matrix-base module

--- lib/lp_engine.dart
+++ lib/lp_engine.dart
@@ -1,6 +1,6 @@
 library engine;
 
-// export 'src/calculate.dart';
 export 'src/classes/terminal_limits.dart';
 export 'src/court.dart';
-export'src/polygons/quadrilateral.dart';
+export 'src/polygons/quadrilateral.dart';
+export 'src/pong_game/court.dart';
--- lib/src/polygons/quadrilateral.dart
+++ lib/src/polygons/quadrilateral.dart
@@ -1,11 +1,11 @@
-import 'package:lp_engine/src/utils/base.dart';
+// import 'package:lp_engine/src/utils/base.dart';
 
-class Quadrilateral extends Base {
-  void create(int width, int height) {
-    createRow(0, width, '=', true);
+// class Quadrilateral extends Base {
+//   void create(int width, int height) {
+//     createRow(0, width, '=', true);
 
-    createArea(0, width, height, 0);
+//     createArea(0, width, height, 0);
 
-    if (height > 1) createRow(0, width, '=', true);
-  }
-}
+//     if (height > 1) createRow(0, width, '=', true);
+//   }
+// }
